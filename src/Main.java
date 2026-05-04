7d0a33a4bbf52497cf74b6d1d9b2397419c3cf79
Andres30Bravo <andres30bravo10@digitechfp.com>
2026-05-04 18:10:59 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,1 +1,2 @@
 public class Main { public static void main(String[] args) { System.out.println("MiniInventario funcionando"); } }
+System.out.println("versión inicial con gestión básica de productos.")
